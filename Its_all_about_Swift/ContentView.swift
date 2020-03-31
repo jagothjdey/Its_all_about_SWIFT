4e156c86b4ba61972d6f5d08126d7ccaa902038c
dey <jagothjyotidey91@gmail.com>
2020-03-31 09:42:51 +0200

    First Commit

--- Its_all_about_Swift/ContentView.swift
+++ Its_all_about_Swift/ContentView.swift
@@ -1,21 +1,12 @@
-//
-//  ContentView.swift
-//  Its_all_about_Swift
-//
-//  Created by Jagoth Jyoti Dey on 31.03.20.
-//  Copyright © 2020 Jagoth. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    init(){
+        
+    }
+    
     var body: some View {
         Text("Hello, World!")
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
