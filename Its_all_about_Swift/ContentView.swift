ecce70887fb531c5a8035157d8deaafca118e7ae
dey <jagothjyotidey91@gmail.com>
2020-03-31 09:47:33 +0200

    Property Observers

--- Its_all_about_Swift/ContentView.swift
+++ Its_all_about_Swift/ContentView.swift
@@ -3,7 +3,21 @@ import SwiftUI
 struct ContentView: View {
     
     init(){
-        
+        //propertyObserver()
+    }
+    
+    func propertyObserver(){
+        var myProperty = 5 {
+            willSet {
+                print("1")
+                print("Will set to \(newValue). It was previously \(myProperty)")
+            }
+            didSet {
+                print("2")
+                print("Did set to \(myProperty). It was previously \(oldValue)")
+            }
+        }
+        myProperty = 6
     }
     
     var body: some View {
