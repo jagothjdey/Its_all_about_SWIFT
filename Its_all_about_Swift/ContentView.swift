379415cd7103238c712d4ff57d0597042faefded
dey <jagothjyotidey91@gmail.com>
2020-03-31 09:40:10 +0200

    Initial Commit

---
+++ Its_all_about_Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Its_all_about_Swift
+//
+//  Created by Jagoth Jyoti Dey on 31.03.20.
+//  Copyright © 2020 Jagoth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
