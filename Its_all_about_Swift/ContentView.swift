8072d9e5881c7133c497b9d5bf3989ef37f428d1
dey <jagothjyotidey91@gmail.com>
2020-04-01 09:38:34 +0200

    closureMap added

--- Its_all_about_Swift/ContentView.swift
+++ Its_all_about_Swift/ContentView.swift
@@ -1,11 +1,45 @@
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
-    
     init(){
         // call any method from below
         // computedProperties()
-        //convertNumberFromString()
+        // convertNumberFromString()
+        // string()
+        // closureMap
+        
+    }
+    
+    func closureMap(){
+        let digitNames = [
+            0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+            5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+        ]
+        let numbers = [16, 58, 510]
+        
+        let strings = numbers.map { (number) -> String in
+            var number = number
+            var output = ""
+            repeat {
+                output = digitNames[number % 10]! + output
+                number /= 10
+            } while number > 0
+            return output
+        }
+        
+        print(strings)
+    }
+    
+    func backward(_ s1: String, _ s2: String) -> Bool {
+        return s1 < s2
+    }
+    
+    
+    func string(){
+        let words = ["apple", "orange", "banana"]
+        let str = words.joined(separator: " & ")
+        print(str) // "apple & orange & banan
     }
     
     func convertNumberFromString(){
